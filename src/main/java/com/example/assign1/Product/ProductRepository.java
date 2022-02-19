e87e00cfaba8d4af4cae7919385bad07ff3bb524
astralaria <pawut.j@gmail.com>
2022-02-19 17:36:19 +0700

    #4-6 Create Product findById + Test

--- src/main/java/com/example/assign1/Product/ProductRepository.java
+++ src/main/java/com/example/assign1/Product/ProductRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
         List<Product> findByProductName(String name);
+        Optional<Product> findById(Long id);
 }
