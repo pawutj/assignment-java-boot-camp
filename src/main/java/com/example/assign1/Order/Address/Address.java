840a784e4d673ac18e34765970033dbe78ed8ec4
astralaria <pawut.j@gmail.com>
2022-02-23 18:52:53 +0700

    Create Adrees repository  (add , findByUserId ) - 15

--- src/main/java/com/example/assign1/Order/Address/Address.java
+++ src/main/java/com/example/assign1/Order/Address/Address.java
@@ -1,10 +1,16 @@
-package com.example.assign1.Order;
+package com.example.assign1.Order.Address;
 
 import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 
 @Data
 public class Address {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String email;
     private String name;
