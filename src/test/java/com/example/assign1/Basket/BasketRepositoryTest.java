b7d670a60733e056ccea5e4d9af218c864110734
astralaria <pawut.j@gmail.com>
2022-02-27 11:13:24 +0700

    Fix : findByUserId Error by SeedDB

--- src/test/java/com/example/assign1/Basket/BasketRepositoryTest.java
+++ src/test/java/com/example/assign1/Basket/BasketRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.assign1.Basket;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.swing.text.html.Option;
 
@@ -11,6 +12,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class BasketRepositoryTest {
 
     @Autowired
